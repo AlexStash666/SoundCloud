5b6402a3ab0b4b985fd4c75242620e07fc868e61
AlexStash666 <alexanderstashinski@gmail.com>
2022-02-17 17:09:09 +0300

    spotify login

--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -4,6 +4,9 @@ from .endpoint import views, auth_views
 
 urlpatterns = [
     path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
+
+    path('spotify-callback/', auth_views.spotify_auth),
+    path('spotify-login/', auth_views.spotify_login),
     path('google/', auth_views.google_auth),
-    path('', auth_views.google_login)
+    path('', auth_views.google_login),
 ]
